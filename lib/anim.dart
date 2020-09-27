1f4f844f0d88ac9cf93afc1e6065292d2525cc99
Amil-Gupta <64407800+Amil-Gupta@users.noreply.github.com>
2020-09-28 00:29:47 +0530

    Add files via upload
    The relevant files required to create the animation app by modifying the initial structure created by the 'flutter create' command

---
+++ lib/anim.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+class Anim extends StatefulWidget {
+  @override
+  _AnimState createState() => _AnimState();
+}
+
+class _AnimState extends State<Anim> with SingleTickerProviderStateMixin{
+  var myanicon;
+  var animation;
+
+  @override
+
+  void initState() {
+    super.initState();
+    myanicon = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    );
+
+    print(myanicon);
+
+    animation = CurvedAnimation(parent: myanicon, curve: Curves.easeOut)
+      ..addListener(() {
+        setState(() {
+          print(animation.value);
+        });
+      });
+
+    print(animation);
+
+    myanicon.forward();
+    print(myanicon);
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    myanicon.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    var h=MediaQuery.of(context).size.height;
+    var w= MediaQuery.of(context).size.width;
+    var v=animation.value;
+    int n=(v*10).round();
+    var cont1=Container(
+      color:Color.fromRGBO(20*n, 20*n, 10*n,1),
+      child:Center(
+        child:Container(
+          height:h*0.884,
+          child: Center(
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  ('HELLO WORLD'.substring(0,n+1))+((v==1)?'.':''),
+                  style: TextStyle(
+                    fontSize: h*v*0.06,
+                    fontWeight: FontWeight.bold,
+                    color:Color.fromRGBO(25*(10-n), 25*(10-n), 25*(10-n), 1),
+                  ),
+                ),
+                Container(
+                  height:(v<1)?(h*0.07*v):0,
+                  width:w*0.05*v,
+                  color:(n%2==0)?Colors.white:Colors.black45,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    var cont2=Container(
+      decoration: BoxDecoration(
+        image:DecorationImage(
+          image: AssetImage(
+            'assets/earth_pic.jpg',
+          ),
+          fit: BoxFit.fill 
+        )
+      ),
+      child:Center(
+        child:Container(
+          height:h*0.884,
+          child: Center(
+            child:Text(
+              'HELLO WORLD.',
+              style: TextStyle(
+                fontSize: h*0.065,
+                fontWeight: FontWeight.bold,
+                color:Colors.redAccent,
+              ),  
+            ),
+          ),
+        ),
+      ),
+    );
+    return Center(
+      child: Container(
+        child: GestureDetector(
+          onTap: () {
+            myanicon.forward(from:0.0);
+          },
+          child: (v<1)?cont1:cont2,
+        ),
+      ),
+    );
+  }
+}
